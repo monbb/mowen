292415fa04eb67ed8699843d442cef69fb2869e9
Yatming <moyiming@tungee.com>
2021-07-27 20:52:49 +0800

    add: configs

---
+++ backend/configs/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    __init__.py.py
+    ~~~~~~~~~~~~~~~~~~~~~~~
+    
+    Description of this file
+    
+    :author: yatming
+    :copyright: (c) 2020
+    :date created: 2021/7/27
+    
+"""
+
+TARGET_URL = "https://www.lecake.com"
