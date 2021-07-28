e519f54e1f658cfba0f004c7fba3e15595050596
Yatming <moyiming@tungee.com>
2021-07-28 21:44:52 +0800

    add: 热销产品

--- backend/logic/sipders/run.py
+++ backend/logic/sipders/run.py
@@ -49,8 +49,25 @@ class TaskExecutor(object):
         cake_ele_nodes = self.parse_html(html_text, xpath_exp)
         for node in cake_ele_nodes:
             img_xpath = './div[@class="item_info"]/div[@class="item_message"]//a[@class="img_link"]//img/@data-original'
-            ele = node.xpath(img_xpath)
-            print(ele)
+            img_url = node.xpath(img_xpath)[0] # 首页图片url
+            label_message_xpath = './/div[@class="item_message"]/a//b'
+            label_info = node.xpath(label_message_xpath)
+            if label_info:
+                label = label_info[0].text # 左上角标签
+            tag_xpath = './/div[@class="item_message"]/div[@class="item_detail"]/div[@class="name_wrap"]//div[@class="p_tag"]/span'
+            tag_list = node.xpath(tag_xpath)
+            if tag_list:
+                tag = tag_list[0].text # 优惠标签
+            title_xpath = './/div[@class="item_message"]/div[@class="item_detail"]/div[@class="name_wrap"]//h3'
+            title_list = node.xpath(title_xpath)
+            title = title_list[0].text # 名称
+            recommend_xpath = './/div[@class="item_message"]/div[@class="item_detail"]/div[@class="name_wrap"]//div[@class="recommend_reason no_wrap"]'
+            recommend_reason_info = node.xpath(recommend_xpath)[0].text.strip() # 推荐原因
+            price_xpath = './/div[@class="item_message"]/div[@class="item_detail"]/div[@class="price_wrap"]/p/text()'
+            price = node.xpath(price_xpath)[1].strip() # 价格
+
+
+
 
 
 if __name__ == '__main__':
